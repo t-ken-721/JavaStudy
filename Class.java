36d447887cf3c9b11f210315ad8c689b9b1d7cfc
t-ken-721 <kenji.soccer721@gmail.com>
2024-05-12 15:46:42 +0900

    勉強した

---
+++ Class.java
@@ -0,0 +1,25 @@
+public class Class {
+    public static void main(String[] args) {
+    // クラス＝データの集合体、様々なデータ型と処理を一つにする
+    // kラス＝オブジェクト
+    // オブジェクトを組み合わせて一つシステムを開発する＝＝オブジェクト指向
+}
+}
+class Test  {
+    Test(int num) {
+        // クラスの実態を作成する時に一番最初に処理されるこーど
+        this.num = num;
+    }
+    
+    // オーバーロード＝同じ名前のメソッドやコンストラクトを複数定義すること、シグネチャ=(hikisは変える必要が得る
+
+
+    // クラス内の変数とメソッドをまとめてメンバという
+    private int num;
+
+
+    public int getNum() {
+        return this.num;
+    }
+
+}
